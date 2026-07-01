93960ea663de6335f9f913e34e0676757b6792b1
rdtngh <raditya.123140125@student.itera.ac.id>
2026-07-01 15:15:15 +0700

    chore: initialize React and Laravel project structure

--- be-in/routes/web.php
+++ be-in/routes/web.php
@@ -3,5 +3,11 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+
+    return [
+        "nama" => "Radit",
+        "umur" => 20,
+        "kampus" => "ITERA"
+    ];
+
 });

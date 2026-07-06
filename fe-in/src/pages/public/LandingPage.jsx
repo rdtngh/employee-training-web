abfdfd9a983a35e7c3736d4165b4729be04f6c9a
beningapniprameswari <beningapniprameswari@gmail.com>
2026-07-06 15:25:37 +0700

    feat: add reusable button component

--- fe-in/src/pages/public/LandingPage.jsx
+++ fe-in/src/pages/public/LandingPage.jsx
@@ -1,7 +1,17 @@
+import Button from "../../components/ui/Button";
+
 function LandingPage() {
   return (
     <div>
+
       <h1>Landing Page</h1>
+
+      <Button>
+
+        Login
+
+      </Button>
+
     </div>
   );
 }

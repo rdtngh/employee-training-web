de68531c88e92f150e5a7200bbf0e7003a54599e
beningapniprameswari <beningapniprameswari@gmail.com>
2026-07-06 08:47:03 +0700

    feat(frontend): configure routing and initialize application pages

--- fe-in/src/App.jsx
+++ fe-in/src/App.jsx
@@ -1,49 +1,7 @@
-import { useEffect, useState } from "react";
-import axios from "axios";
+import AppRouter from "./routes/AppRouter";
 
 function App() {
-
-    const [users, setUsers] = useState([]);
-
-    useEffect(() => {
-
-        axios.get("http://127.0.0.1:8000/api/hello")
-            .then((response) => {
-
-                console.log(response.data);
-
-                setUsers(response.data);
-
-            });
-
-    }, []);
-
-    return (
-
-        <div>
-
-            <h1>Daftar User</h1>
-
-            {
-                users.map((user) => (
-
-                    <div key={user.id}>
-
-                        <h3>{user.nama}</h3>
-
-                        <p>Umur : {user.umur}</p>
-
-                        <hr/>
-
-                    </div>
-
-                ))
-            }
-
-        </div>
-
-    );
-
+    return <AppRouter />;
 }
 
 export default App;
